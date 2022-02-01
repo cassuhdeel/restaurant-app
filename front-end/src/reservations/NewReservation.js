15b7a9b36e5a3907d008faa5e042cf8683687fa5
Cassidy Lim <88170408+cassuhdeel@users.noreply.github.com>
2022-02-01 02:54:24 -0500

    Updated key values
    Updated the key values similar to this file as well

--- front-end/src/reservations/NewReservation.js
+++ front-end/src/reservations/NewReservation.js
@@ -167,7 +167,7 @@ export default function NewReservation({ loadDashboard, edit }) {
   }
 
   const errorsResult = () => {
-    return errors.map((error, idx) => <ErrorAlert key={idx} error={error} />);
+    return errors.map((error) => <ErrorAlert key={new Date().getTime()} error={error} />);
   };
 
   return (
